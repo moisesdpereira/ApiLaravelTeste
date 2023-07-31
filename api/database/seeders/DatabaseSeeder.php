cb24d82803ddd225df513466164f2e7b8f772edc
moisesdpereira <52746034+moisesdpereira@users.noreply.github.com>
2023-07-31 18:22:10 -0300

    Correção Migrate - Seeder - Factories
    Corrigindo o código

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ use App\Models\DoctorPatient;
 use App\Models\Patient;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -26,5 +27,13 @@ class DatabaseSeeder extends Seeder
         Doctor::factory(10)->create();
 
         DoctorPatient::factory(10)->create();
+
+         User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'email_verified_at' => now(),
+             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+             'remember_token' => Str::random(10),
+         ]);
     }
 }
